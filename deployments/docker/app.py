1c7b86311ea98d70a867303df37dc66cbbfa0915
None <husneozmen@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-09 07:18:54 +0000

    added husneozmen page

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -1,19 +1,21 @@
 from flask import Flask,  jsonify 
 import os 
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
     return jsonify({
-        'message': 'hello world',
-        'environment': os.environ.get('ENVIRONMENT'),
+        'message': 'Hello I am a developer my name is Husne Ozmen',
         'environment': os.environ.get('ENVIRONMENT'),
         'owner': 'husneozmen',
         'namespace': os.environ.get('NAMESPACE')
     })
-@app.route('/soon')
+@app.route('/husneozmen')
 def comming_soon():
     return jsonify({
-        'message': 'This is comming soon page!!'
+        'message': 'This is Husne Ozmens page'
     })
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
