34fc1f369ff2030616279d540c9d245924315782
None <husneozmen@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-09 03:23:39 +0000

    new

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -1,1 +1,19 @@
-
+from flask import Flask,  jsonify 
+import os 
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return jsonify({
+        'message': 'hello world',
+        'environment': os.environ.get('ENVIRONMENT'),
+        'environment': os.environ.get('ENVIRONMENT'),
+        'owner': 'husneozmen',
+        'namespace': os.environ.get('NAMESPACE')
+    })
+@app.route('/soon')
+def comming_soon():
+    return jsonify({
+        'message': 'This is comming soon page!!'
+    })
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
