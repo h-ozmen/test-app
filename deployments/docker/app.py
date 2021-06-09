abd4234deb9c02d601bd15b22b200968eba19c2f
None <husneozmen@bastion-fuchicorp-com.us-central1-b.c.geometric-sled-282503.internal>
2021-06-09 07:57:16 +0000

    new

--- deployments/docker/app.py
+++ deployments/docker/app.py
@@ -11,10 +11,17 @@ def hello_world():
         'namespace': os.environ.get('NAMESPACE')
     })
 @app.route('/husneozmen')
-def comming_soon():
+def husneozmen():
     return jsonify({
         'message': 'This is Husne Ozmens page'
     })
+
+@app.route('/soon')
+def comming_soon():
+    return jsonify({
+        'message': 'This is coming soon page!!'
+    })
+
 
 
 if __name__ == '__main__':
